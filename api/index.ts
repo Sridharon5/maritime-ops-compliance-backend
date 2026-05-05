1d105abd584dd6deb81d7bac476f59de449042b7
Sridharon5 <malothsridharvarma@gmail.com>
2026-05-06 00:34:52 +0530

    Updated type

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
+import type { IncomingMessage, ServerResponse } from "node:http";
 import { app } from "../src/app.js";
 import { connectDatabase } from "../src/config/database.js";
 
 let databaseConnection: Promise<void> | undefined;
 
-export default async function handler(req: unknown, res: unknown) {
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
   databaseConnection ??= connectDatabase();
   await databaseConnection;
 
