a185625a0fad71cf68e6ea80686429bf4fb8998e
Sridharon5 <malothsridharvarma@gmail.com>
2026-05-06 01:13:20 +0530

    Uupdated

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,17 @@
 import "dotenv/config";
 import type { IncomingMessage, ServerResponse } from "node:http";
-import { app } from "../src/app.js";
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL ?? "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,x-user-role");
+
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  const { app } = await import("../src/app.js");
   return app(req, res);
 }
